dd4d7e93f8cbb484a784686c920ae52d17e15a99
Alex Braun <alexander.g.braun@gmail.com>
2025-03-24 15:46:27 -0400

    /bin/cat

--- zsh/scripts/variables.sh
+++ zsh/scripts/variables.sh
@@ -1,4 +1,4 @@
 export CREDS_DIR=~/Documents/credentials
 export MISC_DIR=~/Documents/misc
 export PROJECTS_DIR=~/Documents/projects
-export SLACK_URL=`cat $CREDS_DIR/slack-url.txt`
+export SLACK_URL=`/bin/cat $CREDS_DIR/slack-url.txt`
